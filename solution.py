9429a79ace149db89109e6b4196cb0d7034b9264
promax <62617008+promaxV@users.noreply.github.com>
2023-04-13 23:20:41 +0300

    trying anderson-darling

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+#from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 
 chat_id = 664256606
 alpha = 0.05
 
 def solution(x: np.array, y: np.array) -> bool:
-    statistic, p_value = ks_2samp(x, y)
+    _, _, p_value = anderson_ksamp([x, y])
     return p_value < alpha
