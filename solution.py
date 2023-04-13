b78467355ae99472b54b3cce0f223b9f84af8c82
promax <62617008+promaxV@users.noreply.github.com>
2023-04-13 21:41:24 +0300

    trying kstest

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 664256606
+alpha = 0.05
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    statistic, p_value = ks_2samp(x, y)
+    return p_value < alpha
